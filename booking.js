676cf39d3108fcdad1bd7223197d0234b55cf769
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-03-05 09:42:34 +0530

    FIX: number range for seatID (1-5)

--- booking.js
+++ booking.js
@@ -2,12 +2,12 @@
 const bookings = [];
 
 function chkValidEntry(element){
-        return /^[A-D](10|[1-9])$/.test(element);
+        return /^[A-D](10|[1-5])$/.test(element);
 }
 
 function chkAvailability(seatID, seat_array){
-    seatObjFound = seat_array.find((so)=>so.id === seatID);
-    return seatObjFound.status == "available";
+    seatObjFound = seat_array.find((so) => so.id === seatID);
+    return (seatObjFound.status == "available");
 }
 
 module.exports = {bookings, chkValidEntry, chkAvailability};
